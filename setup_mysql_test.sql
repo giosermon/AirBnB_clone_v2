34b274e2b9b53912787426835b9c1f4b1bfc09d2
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-08-09 11:38:40 -0500

    task4: create database of testing and set privileges

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Create testing database
+-- set database and privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
+
+GRANT ALL ON hbnb_test_db . * TO 'hbnb_test'@'localhost'
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost'
+
