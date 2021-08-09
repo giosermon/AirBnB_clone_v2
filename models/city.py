f6b26a3116b32693c6282cb70e2648b27344a6a3
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-08-09 23:12:30 +0000

    task6: test with foreign key

--- models/city.py
+++ models/city.py
@@ -11,4 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
-    state_id = Column(String, ForeignKey("States.id"), nullable=False)
+    # state_id = Column(String, ForeignKey("States.id"), nullable=False)
