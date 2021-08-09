c090140028308f928f081779da60e2086d147f9c
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-08-09 11:27:06 -0500

    task3: create table and set grant privileges

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python3
-"""This module tests console.py file.
-Usage:
-    To be used with the unittest module:
-    "python3 -m unittest discover tests" command or
-    "python3 -m unittest tests/test_console.py"
 """
+Console testing Suite
+"""
+
+from console import HBNBCommand
 import unittest
+from unittest.mock import create_autospec, patch
+import sys
+from io import StringIO
 import os
 
 
+MSG_MISSING = '** class name missing **'
+
+
 class TestConsole(unittest.TestCase):
-    ''' TestCase class for storing the unittests of the console. '''
 
-    def test_create(self):
-        ''' Tests for the create command. '''
-        # Create console session.
-
-    @classmethod
-    def tearDown(self):
-        ''' Removes the file.json on each test. '''
+    def teardown(cls):
+        ''' Delete File'''
         try:
             os.remove("file.json")
         except:
             pass
 
+    def setUp(self):
+        ''' Sets up the mock stdin and stderr. '''
+        self.mock_stdout = create_autospec(sys.stdout)
 
-if __name__ == "__main__":
-    unittest.main()
+    def get_cli(self):
+        '''create console instance with mock in and out'''
+        stdin = create_autospec(sys.stdin)
+        stdout = create_autospec(sys.stdin)
+        return HBNBCommand(stdin=stdin, stdout=stdout)
+
+    def test_create(self):
+        ''' Tests for the create command. '''
+        cli = self.create_session()
+        with patch('sys.stdout', new=StringIO()) as Output:
+            output = Output.getvalue().strip()
+            self.assertFalse(cli.onecmd('create'))
+
+        self.assertEqual(MSG_MISSING, output)
