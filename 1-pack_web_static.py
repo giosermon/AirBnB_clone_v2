d98a1397966469dd3239cad312eac9b3b2348dff
william.rodriguez <william.rodriguez@mercadolibre.com.co>
2021-08-26 20:26:04 +0000

    fix version path

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """ compress folder with fabric module """
-from os import path
-from fabric.api import local
-from datetime import datetime
 
 VERSION_PATH = "./versions"
 MKDIR_FOLDER = "mkdir {}"
@@ -11,16 +8,18 @@ DATE_FORMAT = "%Y%m%d%H%M%S"
 
 def do_pack():
     """ Compress web_static folder"""
-    # validate folder
+    from os import path
+    from fabric.api import local
+    from datetime import datetime
+
     if not path.exists(VERSION_PATH):
         local(MKDIR_FOLDER.format(VERSION_PATH))
-    # get now and get format to filename
+
     current_time = datetime.now()
     date_format = current_time.strftime(DATE_FORMAT)
     filename = "web_static_{}.tgz".format(date_format)
-    # compress
     try:
         local("tar -cvzf {}/{} /data/web_static/".format(VERSION_PATH, filename))
         return filename
-    except:
+    except Exception:
         return None
