4e291cef306acbe08e157afa398bafda95404219
william.rodriguez <william.rodriguez@mercadolibre.com.co>
2021-08-26 19:44:36 +0000

    fix sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,17 +6,15 @@ apt-get install nginx -y
 
 # set nginx config
 CONFIG_PATH=/etc/nginx/sites-available/default
-OLD="server_name localhost;"
 NEW="location /hbnb_static {alias /data/web_static/current/;}"
-sed -i "/$OLD/a\\$NEW" $CONFIG_PATH
+sed -i "29i $NEW" $CONFIG_PATH
 
 # create folders
-mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/current
+mkdir -p /data/web_static/releases/test/
 # Load html content
 echo "Fake content" >/data/web_static/releases/test/index.html
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu /data
-
+chown -R ubuntu:ubuntu /data
 service nginx restart
