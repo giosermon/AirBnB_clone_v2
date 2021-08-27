043d7c243e4aba1175ec9a78290c2ae7b8e23355
giosermon <2172@holbertonschool.com>
2021-08-26 22:03:55 -0500

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,43 +10,46 @@ from datetime import datetime
 import os
 from fabric.api import run, put, env
 
-env.use_ssh_config = True
 env.hosts = ['ubuntu@34.139.216.89', 'ubuntu@54.159.25.183']
 
 
 
-def do_pack():
-    """Packs the folder web_static
-    """
-    now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    outpath = "./versions/web_static_{}".format(now)
-    filename = "{}.tgz".format(outpath)
-    local('mkdir -p ./versions')
-    local("tar -zcvf '{}' web_static".format(filename))
-
-    if os.path.exists(filename):
-        return outpath
-    else:
-        return None
-
-
 def do_deploy(archive_path):
-    """Deploys to the nodes
     """
-    if os.path.exists(archive_path):
-        tar_name = re.search('web_static_[0-9]*.tgz', archive_path).group(0)
-        untar_path = "/data/web_static/releases/{}"\
-            .format(tar_name.replace('.tgz', ''))
+    Distributes an archive to the web servers
+    """
+    # Validate if the archive path does not exist
+    if not os.path.exists(archive_path):
+        return False
+    try:
 
-        put(archive_path, '/tmp')
-        run("mkdir -p {}".format(untar_path))
-        run("tar -zxf /tmp/{} -C {}".format(tar_name, untar_path))
-        run("rm /tmp/{}".format(tar_name))
-        run("mv {}/web_static/* {}".format(untar_path, untar_path))
-        run("rm -rf {}/web_static".format(untar_path))
+        split_name = archive_path.split('/')
+        file_name = split_name[1]
+
+
+        put(archive_path, "/tmp/")
+
+
+        file_path = file_name.split('.')
+        file_path = '/data/web_static/releases/' + file_path[0]
+        run("mkdir -p {}".format(file_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, file_path))
+
+
+        run("rm /tmp/{}".format(file_name))
+
+
+        file_path_move = file_path + '/web_static'
+        run("mv {}/* {}".format(file_path_move, file_path))
+
+
+        run("rm -rf {}".format(file_path_move))
+
+
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(untar_path))
-        print('New version deployed!')
+
+
+        run("ln -s {} /data/web_static/current".format(file_path))
         return True
-    else:
+    except:
         return False
