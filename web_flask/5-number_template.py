d860abba1292198ff1695d16e04545d77d46ce02
giosermon <2172@holbertonschool.com>
2021-09-12 20:22:48 -0500

    add task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""starts flask"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cfun(text):
+    """returns"""
+    return 'C %s' % text.replace("_", " ")
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoncool(text='is cool'):
+    """returns"""
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numver(n):
+    """returns"""
+    return '%d is a number' % n
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_templates(n):
+    """returns"""
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
